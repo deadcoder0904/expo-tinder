f3a5878f290fb4972209b311a3ac7276e51439cc
Akshay Kadam <akshaykadam0904@gmail.com>
2019-05-19 11:17:47 +0530

    Change TabBarIcon

--- components/TabBarIcon.js
+++ components/TabBarIcon.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import { Icon } from 'expo';
-
-import Colors from '../constants/Colors';
+import React from 'react'
+import Colors from '../constants/Colors'
 
 export default class TabBarIcon extends React.Component {
   render() {
+    const { Icon, name, focused } = this.props
     return (
-      <Icon.Ionicons
-        name={this.props.name}
+      <Icon
+        name={name}
         size={26}
         style={{ marginBottom: -3 }}
-        color={this.props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+        color={focused ? Colors.tabIconSelected : Colors.tabIconDefault}
       />
-    );
+    )
   }
 }
