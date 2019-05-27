0246824b35da2c49683adc48e301d44ed1ad6240
Akshay Kadam <akshaykadam0904@gmail.com>
2019-05-27 16:45:21 +0530

    Make a Card

---
+++ components/Card.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Platform, StyleSheet } from 'react-native'
+import { Tile } from 'react-native-elements'
+import Layout from '../constants/Layout'
+
+const BOTTOM_BAR_HEIGHT = !Platform.isPad ? 29 : 49 // found from https://stackoverflow.com/a/50318831/6141587
+
+export const Card = ({ pic, title, caption }) => (
+  <Tile
+    imageSrc={pic}
+    imageContainerStyle={styles.imageContainer}
+    activeOpacity={0.9}
+    title={title}
+    titleStyle={styles.title}
+    caption={caption}
+    captionStyle={styles.caption}
+    containerStyle={styles.container}
+    featured
+  />
+)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  imageContainer: {
+    width: Layout.window.width - 30,
+    height: Layout.window.height - BOTTOM_BAR_HEIGHT * 6,
+    borderRadius: 10,
+    overflow: 'hidden', // this does magic
+  },
+  title: {
+    position: 'absolute',
+    left: 10,
+    bottom: 30,
+  },
+  caption: {
+    position: 'absolute',
+    left: 10,
+    bottom: 10,
+  },
+})
