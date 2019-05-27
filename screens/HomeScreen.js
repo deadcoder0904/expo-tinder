426db0fbcc5c7f6afa99b8d3f3d2a1d3b8b39839
Akshay Kadam <akshaykadam0904@gmail.com>
2019-05-27 16:46:10 +0530

    Add HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import React from 'react'
+import { SafeAreaView } from 'react-native'
+import Swiper from 'react-native-realistic-deck-swiper'
+
+import { Card } from '../components/Card'
+import Pics from '../constants/Pics'
+
 class HomeScreen extends React.Component {
   render() {
     return (
       <SafeAreaView>
-        <Text>Home Screen</Text>
+        <Swiper
+          cardsData={Pics}
+          renderCard={Card}
+          offsetAngleMin={-2}
+          offsetAngleMax={2}
+        />
       </SafeAreaView>
     )
   }
