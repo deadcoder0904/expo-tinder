383bd4c2ac61ea976ae80323de698899cb0de240
Akshay Kadam <akshaykadam0904@gmail.com>
2019-05-27 17:26:20 +0530

    Rename Pics to HomeScreenPics & TopPicksScreenPics

--- constants/Pics.js
+++ constants/Pics.js
@@ -1,6 +1,6 @@
 import shuffleArray from '../utils/shuffleArray'
 
-const Pics = shuffleArray([
+export const HomeScreenPics = shuffleArray([
   {
     pic: require('../assets/images/women/women1.jpg'),
     title: 'Amelia, 27',
@@ -103,4 +103,55 @@ const Pics = shuffleArray([
   },
 ])
 
-export default Pics
+export const TopPicksScreenPics = shuffleArray([
+  {
+    pic: require('../assets/images/women/women11.jpg'),
+    title: 'Annie, 40',
+    caption: '26h left',
+  },
+  {
+    pic: require('../assets/images/women/women12.jpg'),
+    title: 'Lena, 31',
+    caption: '20h left',
+  },
+  {
+    pic: require('../assets/images/women/women13.jpg'),
+    title: 'Kendra, 19',
+    caption: '15h left',
+  },
+  {
+    pic: require('../assets/images/women/women14.jpg'),
+    title: 'Mia, 23',
+    caption: '45h left',
+  },
+  {
+    pic: require('../assets/images/women/women15.jpg'),
+    title: 'Jenny, 27',
+    caption: '12h left',
+  },
+  {
+    pic: require('../assets/images/men/men11.jpg'),
+    title: 'Dwayne, 34',
+    caption: '13h left',
+  },
+  {
+    pic: require('../assets/images/men/men12.jpg'),
+    title: 'Novak, 27',
+    caption: '22h left',
+  },
+  {
+    pic: require('../assets/images/men/men13.jpg'),
+    title: 'Zikomo, 32',
+    caption: '20h left',
+  },
+  {
+    pic: require('../assets/images/men/men14.jpg'),
+    title: 'Sam, 19',
+    caption: '42h left',
+  },
+  {
+    pic: require('../assets/images/men/men15.jpg'),
+    title: 'Richard, 31',
+    caption: '21h left',
+  },
+])
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,14 +3,14 @@ import { SafeAreaView } from 'react-native'
 import Swiper from 'react-native-realistic-deck-swiper'
 
 import { Card } from '../components/Card'
-import Pics from '../constants/Pics'
+import { HomeScreenPics } from '../constants/Pics'
 
 class HomeScreen extends React.Component {
   render() {
     return (
       <SafeAreaView>
         <Swiper
-          cardsData={Pics}
+          cardsData={HomeScreenPics}
           renderCard={Card}
           offsetAngleMin={-2}
           offsetAngleMax={2}
