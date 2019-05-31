e0f1ab0c78d3b906a629323a677b96be51783f76
Akshay Kadam <akshaykadam0904@gmail.com>
2019-05-31 15:06:12 +0530

    Top Picks Screen complete 💯

--- screens/TopPicksScreen.js
+++ screens/TopPicksScreen.js
@@ -1,14 +1,69 @@
 import React from 'react'
-import { SafeAreaView, Text } from 'react-native'
+import { ScrollView, StyleSheet, View } from 'react-native'
+import { Text, Tile } from 'react-native-elements'
+import { SafeAreaView } from 'react-navigation'
+import { TopPicksScreenPics } from '../constants/Pics'
 
 class TopPicksScreen extends React.Component {
   render() {
     return (
       <SafeAreaView>
-        <Text>Top Picks Screen</Text>
+        <ScrollView>
+          <Text h2 h2Style={styles.h2Style}>
+            Top Picks
+          </Text>
+          <Text h4 h4Style={styles.h4Style}>
+            Featured profiles of the day, picked just for you
+          </Text>
+          <View style={styles.grid}>
+            {TopPicksScreenPics.map(({ pic, title, caption }, i) => (
+              <Tile
+                imageSrc={pic}
+                activeOpacity={0.9}
+                title={title}
+                titleStyle={styles.title}
+                caption={caption}
+                captionStyle={styles.caption}
+                featured
+                key={title}
+              />
+            ))}
+          </View>
+        </ScrollView>
       </SafeAreaView>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  h2Style: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  h4Style: {
+    textAlign: 'center',
+    color: 'grey',
+  },
+  grid: {
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  title: {
+    position: 'absolute',
+    left: 10,
+    bottom: 50,
+    backgroundColor: 'black',
+    marginBottom: -2,
+    padding: 10,
+  },
+  caption: {
+    position: 'absolute',
+    left: 10,
+    bottom: 0,
+    backgroundColor: 'black',
+    marginTop: 10,
+    padding: 10,
+  },
+})
 
 export default TopPicksScreen
