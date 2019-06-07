469dfc891b37006f419b3dbefbfacf9745ceb523
Akshay Kadam <akshaykadam0904@gmail.com>
2019-06-07 11:28:29 +0530

    Install react-native-deck-swiper

--- components/Card.js
+++ components/Card.js
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
   imageContainer: {
     width: Layout.window.width - 30,
     height: Layout.window.height - BOTTOM_BAR_HEIGHT * 6,
-    borderRadius: 10,
+    borderRadius: 20,
     overflow: 'hidden', // this does magic
   },
   title: {
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,23 +1,31 @@
 import React from 'react'
-import { SafeAreaView } from 'react-native'
-import Swiper from 'react-native-realistic-deck-swiper'
-
+import { SafeAreaView, StyleSheet } from 'react-native'
+import Swiper from 'react-native-deck-swiper'
 import { Card } from '../components/Card'
 import { HomeScreenPics } from '../constants/Pics'
 
 class HomeScreen extends React.Component {
   render() {
     return (
-      <SafeAreaView>
+      <SafeAreaView style={styles.container}>
         <Swiper
-          cardsData={HomeScreenPics}
+          cards={HomeScreenPics}
           renderCard={Card}
-          offsetAngleMin={-2}
-          offsetAngleMax={2}
+          infinite
+          backgroundColor="white"
+          cardHorizontalMargin={0}
+          stackSize={2}
         />
       </SafeAreaView>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'transparent',
+  },
+})
 
 export default HomeScreen
